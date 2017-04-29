4db2ea369942bce09f0e64813e76ffa5aec46e2e
eetuseta <eetu.kaivola@siili.com>
2017-04-29 09:08:48 +0300

    added short about page

---
+++ app/components/About.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+
+var About = (props) => {
+  return (
+    <div>
+      <h1 className="text-center page-title">Mitä</h1>
+      <p>
+        Tämä sivu on tehty kaikille niille joille tasa-arvoinen avioliittolaki ei ole
+        politiikan väline, vaan pikemminkin mittari, kansakunnan sivistykseen.
+
+        Lisätietoja halutessaan löytyy vaikkapa Setan sivuilta.
+
+     </p>
+      <p>
+        Mahdollisesti hyödyllisiä linkkejä.
+      </p>
+      <ul>
+        <li>
+          <a href="https://www.seta.fi">Seta</a>
+        </li>
+
+      </ul>
+    </div>
+  )
+};
+
+module.exports = About;
